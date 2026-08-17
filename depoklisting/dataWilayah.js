49953f867f4c486c202ebfd670038a7a2f983b5e
situslaundry <situslaundry@gmail.com>
2026-08-17 21:22:06 +0700

    Create dataWilayah.js

---
+++ depoklisting/dataWilayah.js
@@ -0,0 +1,64 @@
+window.dataWilayah = window.dataWilayah || {
+      "kota-depok": {
+        slug: "kota-depok",
+        name: "Kota Depok",
+        type: "Kota",
+        kecamatan: {
+          "beji": {
+            name: "Beji",
+            deskripsi: "Kawasan sentra pendidikan di sekitar kampus Universitas Indonesia (UI) dan jalan Margonda Raya. Melayani jemput antar cepat untuk kost mahasiswa dan apartemen.",
+            desa: ["Beji", "Beji Timur", "Kemiri Muka", "Kukusan", "Pondok Cina", "Tanah Baru"]
+          },
+          "pancoran-mas": {
+            name: "Pancoran Mas",
+            deskripsi: "Pusat pemerintahan dan kawasan cagar budaya Depok Lama dekat Stasiun Depok Baru dan Stasiun Depok.",
+            desa: ["Depok", "Depok Jaya", "Mampang", "Pancoran Mas", "Rangkapan Jaya", "Rangkapan Jaya Baru"]
+          },
+          "sukmajaya": {
+            name: "Sukmajaya",
+            deskripsi: "Kawasan pemukiman padat Depok II Tengah dan Depok II Timur dengan fasilitas kurir penjemputan harian.",
+            desa: ["Abadijaya", "Bakti Jaya", "Cisalak", "Mekar Jaya", "Sukmajaya", "Tirtajaya"]
+          },
+          "cimanggis": {
+            name: "Cimanggis",
+            deskripsi: "Kawasan strategis koridor Jl. Raya Bogor dan akses tol Jagorawi/Cijago dengan layanan laundry kilat keluarga.",
+            desa: ["Curug", "Harjamukti", "Cisalak Pasar", "Mekarsari", "Pasir Gunung Selatan", "Tugu"]
+          },
+          "cinere": {
+            name: "Cinere",
+            deskripsi: "Kawasan pemukiman premium dan perbatasan Jakarta Selatan dengan layanan laundry dry clean dan cuci satuan.",
+            desa: ["Cinere", "Gandul", "Pangkalan Jati", "Pangkalan Jati Baru"]
+          },
+          "sawangan": {
+            name: "Sawangan",
+            deskripsi: "Kawasan hunian modern yang berkembang pesat di barat Kota Depok dengan armada jemput antar tepat waktu.",
+            desa: ["Bedahan", "Cinangka", "Kedaung", "Pasir Putih", "Pengasinan", "Sawangan Baru", "Sawangan"]
+          },
+          "bojongsari": {
+            name: "Bojongsari",
+            deskripsi: "Kawasan perbatasan Depok-Bogor-Tangerang Selatan dengan paket laundry kiloan hemat dan cuci karpet masjid.",
+            desa: ["Bojongsari", "Bojongsari Baru", "Curug", "Duren Mekar", "Duren Seribu", "Pondok Petir", "Serua"]
+          },
+          "cilodong": {
+            name: "Cilodong",
+            deskripsi: "Area berkembang di sekitar Grand Depok City (GDC) dan Kostrad dengan layanan cuci bersih wangi tahan lama.",
+            desa: ["Cikumpa", "Cilodong", "Jatimulya", "Kalibaru", "Kalimulya", "Sukamaju"]
+          },
+          "cipayung": {
+            name: "Cipayung",
+            deskripsi: "Kawasan hunian dekat Stasiun Citayam dengan layanan 1 mesin 1 pelanggan tidak dicampur.",
+            desa: ["Boojong Pondok Terong", "Cipayung", "Cipayung Jaya", "Pondok Jaya", "Ratujaya"]
+          },
+          "tapos": {
+            name: "Tapos",
+            deskripsi: "Kawasan hunian timur Kota Depok dengan standar pengerjaan cepat dan pengering gas modern anti apek.",
+            desa: ["Cilangkap", "Cimpaeun", "Jatijajar", "Leuwinanggung", "Sukamaju Baru", "Sukatani", "Tapos"]
+          },
+          "limo": {
+            name: "Limo",
+            deskripsi: "Area berkembang dekat akses tol Desari penghubung Depok-Antasari Jakarta Selatan.",
+            desa: ["Grogol", "Krukut", "Limo", "Meruyung"]
+          }
+        }
+      }
+    };
