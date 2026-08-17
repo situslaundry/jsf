875b03a5ebeb991da9d040dfdd16b56ea8c91ee8
situslaundry <situslaundry@gmail.com>
2026-08-17 19:59:07 +0700

    Create dataWilayah.js

---
+++ bekasilisting/dataWilayah.js
@@ -0,0 +1,191 @@
+window.dataWilayah = window.dataWilayah || {
+      "kota-bekasi": {
+        slug: "kota-bekasi",
+        name: "Kota Bekasi",
+        type: "Kota",
+        kecamatan: {
+          "bekasi-barat": {
+            name: "Bekasi Barat",
+            deskripsi: "Kawasan hunian dan sentra niaga strategis Kota Bekasi dekat akses Tol Jakarta-Cikampek dan Stasiun Kranji.",
+            desa: ["Bintara", "Bintara Jaya", "Jaka Sampurna", "Kota Baru", "Kranji"]
+          },
+          "bekasi-timur": {
+            name: "Bekasi Timur",
+            deskripsi: "Pusat aktivitas perkantoran, perumahan, dan stasiun transit utama Kota Bekasi.",
+            desa: ["Aren Jaya", "Bekasi Jaya", "Duren Jaya", "Margahayu"]
+          },
+          "bekasi-utara": {
+            name: "Bekasi Utara",
+            deskripsi: "Kawasan pemukiman padat Summarecon Bekasi dan sekitarnya dengan layanan kurir jemput cepat.",
+            desa: ["Harapan Baru", "Harapan Jaya", "Kaliabang Tengah", "Marga Mulya", "Perwira", "Teluk Pucung"]
+          },
+          "bekasi-selatan": {
+            name: "Bekasi Selatan",
+            deskripsi: "Pusat bisnis Mall Grand Metropolitan, Galaxy, dan perumahan modern dengan layanan laundry express.",
+            desa: ["Jaka Mulya", "Jaka Setia", "Kayuringin Jaya", "Pekayon Jaya"]
+          },
+          "rawalumbu": {
+            name: "Rawalumbu",
+            deskripsi: "Kawasan pemukiman keluarga mapan dan sentra kuliner dengan layanan jemput antar gratis.",
+            desa: ["Bojong Menteng", "Bojong Rawalumbu", "Pengasinan", "Sepanjang Jaya"]
+          },
+          "medan-satria": {
+            name: "Medan Satria",
+            deskripsi: "Area sentra industri dan hunian Harapan Indah Bekasi dengan jadwal penjemputan harian.",
+            desa: ["Harapan Mulya", "Kali Baru", "Medan Satria", "Pejuang"]
+          },
+          "pondok-gede": {
+            name: "Pondok Gede",
+            deskripsi: "Pintu masuk perbatasan Jakarta Timur-Bekasi dengan mobilitas masyarakat yang sangat tinggi.",
+            desa: ["Jatibening", "Jatibening Baru", "Jaticempaka", "Jatimakmur", "Jatiwaringin"]
+          },
+          "pondok-melati": {
+            name: "Pondok Melati",
+            deskripsi: "Kawasan residensial berkembang di selatan Kota Bekasi dengan paket laundry bulanan hemat.",
+            desa: ["Jatimelati", "Jatimurni", "Jatirahayu", "Jatiwarna"]
+          },
+          "jatisampurna": {
+            name: "Jatisampurna",
+            deskripsi: "Kawasan Cibubur-Bekasi yang asri dengan layanan cuci sepatu, bedcover, dan laundry kiloan kilat.",
+            desa: ["Jatikarya", "Jatiraden", "Jatirangga", "Jatiranggon", "Jatisampurna"]
+          },
+          "jatiasih": {
+            name: "Jatiasih",
+            deskripsi: "Kawasan perumahan berkembang pesat di koridor tol JORR dengan armada antar jemput gesit.",
+            desa: ["Jatiasih", "Jatikramat", "Jatikiwul", "Jatimekar", "Jatirasa", "Jatisari"]
+          },
+          "bantargebang": {
+            name: "Bantargebang",
+            deskripsi: "Area selatan Kota Bekasi dengan standar cuci higienis 1 mesin 1 pelanggan.",
+            desa: ["Bantargebang", "Ciketing Udik", "Cikiwul", "Sumur Batu"]
+          },
+          "mustikajaya": {
+            name: "Mustikajaya",
+            deskripsi: "Kawasan perumahan Grand Wisata dan Dukuh Zamrud dengan layanan prioritas express same-day.",
+            desa: ["Cimuning", "Mustikajaya", "Mustikasari", "Padurenan"]
+          }
+        }
+      },
+      "kabupaten-bekasi": {
+        slug: "kabupaten-bekasi",
+        name: "Kabupaten Bekasi",
+        type: "Kabupaten",
+        kecamatan: {
+          "tambun-selatan": {
+            name: "Tambun Selatan",
+            deskripsi: "Kawasan pemukiman terbesar di Kabupaten Bekasi dekat stasiun Tambun dan akses tol Grand Wisata.",
+            desa: ["Jatimulya", "Lambangjaya", "Lambangsari", "Mangunjaya", "Setiadarma", "Setiamekar", "Sumberjaya", "Tambun", "Tridaya Sakti"]
+          },
+          "tambun-utara": {
+            name: "Tambun Utara",
+            deskripsi: "Kawasan hunian baru berkembang dengan layanan laundry bersih wangi tahan lama.",
+            desa: ["Bagasasi", "Jelegong", "Karangsatria", "Satria Jaya", "Satria Mekar", "Sriamur", "Srijaya", "Srimahi"]
+          },
+          "cibitung": {
+            name: "Cibitung",
+            deskripsi: "Sentra kawasan industri MM2100 dan hunian komuter dengan layanan laundry kilat siap pakai.",
+            desa: ["Cibuntu", "Kertamukti", "Muktiwari", "Sarimukti", "Sukajaya", "Wanajaya", "Wanasari"]
+          },
+          "cikarang-pusat": {
+            name: "Cikarang Pusat",
+            deskripsi: "Pusat Pemerintahan Kabupaten Bekasi (Deltamas) dengan layanan laundry ekspres untuk kantor dan hunian.",
+            desa: ["Cicau", "Hegarmukti", "Jayamukti", "Pasirranji", "Pasirtanjung", "Sukamahi"]
+          },
+          "cikarang-barat": {
+            name: "Cikarang Barat",
+            deskripsi: "Jantung kawasan industri MM2100 dan Gandamekar dengan fasilitas antar jemput shift kerja.",
+            desa: ["Cikedokan", "Danau Indah", "Gandamekar", "Gandasari", "Jatiwangi", "Kalijaya", "Mekawangi", "Sukadanau", "Telaga Asih", "Telagamurni", "Telajung"]
+          },
+          "cikarang-utara": {
+            name: "Cikarang Utara",
+            deskripsi: "Pusat niaga Jababeka, Stasiun Cikarang, dan perkuliahan President University.",
+            desa: ["Cikarang Kota", "Harjamekar", "Karangasih", "Karangbaru", "Karangraharja", "Mekarmukti", "Pasirgombong", "Simpangan", "Tanjungsari", "Waluya", "Wangunharja"]
+          },
+          "cikarang-selatan": {
+            name: "Cikarang Selatan",
+            deskripsi: "Kawasan Lippo Cikarang, EJIP, dan Hyundai dengan layanan laundry standar hotel dan apartemen.",
+            desa: ["Ciantra", "Cibatu", "Pasirsari", "Sukadami", "Sukaresmi", "Serang"]
+          },
+          "cikarang-timur": {
+            name: "Cikarang Timur",
+            deskripsi: "Area Stadion Wibawa Mukti dan perumahan baru dengan layanan cuci kasur, karpet, dan pakaian kiloan.",
+            desa: ["Cipayung", "Hegantara", "Jatibaru", "Jatireja", "Karangsari", "Labansari", "Sertajaya", "Tanjungbaru"]
+          },
+          "babelan": {
+            name: "Babelan",
+            deskripsi: "Kawasan perumahan padat di utara Bekasi dengan layanan kurir jemput harian tepat waktu.",
+            desa: ["Babelan Kota", "Bahagia", "Buni Bakti", "Hurip Jaya", "Kebalen", "Kedung Jaya", "Kedung Pengawas", "Muara Bakti", "Pantai Hurip"]
+          },
+          "tarumajaya": {
+            name: "Tarumajaya",
+            deskripsi: "Kawasan pesisir dan Marunda Center dekat akses Tol Cibitung-Cilincing.",
+            desa: ["Pahlawan Setia", "Pantai Makmur", "Pusaka Rakyat", "Samudra Jaya", "Segara Jaya", "Segaramakmur", "Setia Asih", "Setia Mulya"]
+          },
+          "setu": {
+            name: "Setu",
+            deskripsi: "Kawasan asri berkembang penghubung Bekasi-Cileungsi/Bogor dengan paket laundry keluarga.",
+            desa: ["Burangkeng", "Cibening", "Cijengkol", "Cikarageman", "Lubangbuaya", "Muktiwari", "Ragamanunggal", "Tamansari"]
+          },
+          "serang-baru": {
+            name: "Serang Baru",
+            deskripsi: "Kawasan perumahan Mega Regency dan sentra hunian asri dengan kurir jemput antar.",
+            desa: ["Cilangkara", "Jayamulya", "Jayasampurna", "Nagacipta", "Nagasari", "Sirnajaya", "Sukarragam", "Sukassari"]
+          },
+          "sukatani": {
+            name: "Sukatani",
+            deskripsi: "Sentra pemukiman jalur utara Kabupaten Bekasi dengan harga kiloan ramah kantong.",
+            desa: ["Banjarsari", "Sukamulya", "Sukanyata", "Sukardi", "Sukasedang", "Sukajadi", "Sukasono"]
+          },
+          "karangbahagia": {
+            name: "Karangbahagia",
+            deskripsi: "Area penyangga Cikarang Utara dengan layanan jemput cucian kiloan dan cuci karpet masjid.",
+            desa: ["Karanganyar", "Karangbahagia", "Karangmukti", "Karangrahayu", "Karangsentosa", "Karangsetia", "Sukajaya"]
+          },
+          "kedungwaringin": {
+            name: "Kedungwaringin",
+            deskripsi: "Perbatasan timur Bekasi-Karawang dengan layanan cuci bersih wangi bergaransi.",
+            desa: ["Bojongsari", "Karangharja", "Karangmekar", "Karangsambung", "Kedungwaringin", "Mekarjaya", "Waringinjaya"]
+          },
+          "pebayuran": {
+            name: "Pebayuran",
+            deskripsi: "Kawasan agraris timur laut Bekasi dengan layanan cuci bedcover tebal dan pakaian harian.",
+            desa: ["Bantarjaya", "Bantarsari", "Karangharja", "Karanghaur", "Karangjaya", "Karangpatri", "Karangreja", "Karangsegar", "Kertajaya", "Kertasari", "Sumberreja", "Sumbersari", "Sumberurip"]
+          },
+          "cabangbungin": {
+            name: "Cabangbungin",
+            deskripsi: "Layanan cuci higienis bebas apek menggunakan pengering gas berteknologi tinggi.",
+            desa: ["Jayabakti", "Jayalaksana", "Lenggahjaya", "Lenggahsari", "Setialaksana", "Setiajaya", "Sindangjaya", "Sindangsari"]
+          },
+          "muaragembong": {
+            name: "Muaragembong",
+            deskripsi: "Ujung utara pesisir Bekasi dengan standar penjemputan terjadwal dan hasil wangi berkelas.",
+            desa: ["Pantai Bahagia", "Pantai Bakti", "Pantai Harapanjaya", "Pantai Mekar", "Pantai Sederhana"]
+          },
+          "sukakarya": {
+            name: "Sukakarya",
+            deskripsi: "Kawasan pemukiman asri di jalur utara dengan layanan cuci satuan sepatu dan kiloan express.",
+            desa: ["Sukajadi", "Sukakarya", "Sukalaksana", "Sukamakmur", "Sukamurni", "Sukareja", "Sukaterbit"]
+          },
+          "sukawangi": {
+            name: "Sukawangi",
+            deskripsi: "Area pemukiman berkembang dengan jangkauan antar jemput langsung ke depan pintu rumah.",
+            desa: ["Sukabudi", "Sukadaya", "Sukakerta", "Sukamekar", "Sukatenang", "Sukaringin", "Sukawangi"]
+          },
+          "bojongmangu": {
+            name: "Bojongmangu",
+            deskripsi: "Wilayah selatan Kabupaten Bekasi dekat kawasan GIIC Deltamas dengan layanan cuci kilat.",
+            desa: ["Bojongmangu", "Karangmulya", "Karangindah", "Medalkrisna", "Sukabungah", "Sukamukti"]
+          },
+          "cibarusah": {
+            name: "Cibarusah",
+            deskripsi: "Ujung selatan Kabupaten Bekasi berbatasan dengan Jonggol Bogor dengan paket laundry hemat.",
+            desa: ["Cibarusahjaya", "Cibarusahkota", "Ridogalih", "Ridomanah", "Sindangmulya", "Sirnajati", "Wibawamulya"]
+          },
+          "tambelang": {
+            name: "Tambelang",
+            deskripsi: "Kawasan penghubung jalur tengah Bekasi dengan sistem 1 mesin 1 pelanggan tidak dicampur.",
+            desa: ["Rakitjaya", "Sukabakti", "Sukamaju", "Sukamantri", "Sukarahayu", "Sukarapi", "Sukatani"]
+          }
+        }
+      }
+    };
