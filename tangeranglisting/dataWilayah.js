84fa91e56b35c619bb30ddc1f1b448e626aabeea
situslaundry <situslaundry@gmail.com>
2026-08-18 09:49:28 +0700

    Create dataWilayah.js

---
+++ tangeranglisting/dataWilayah.js
@@ -0,0 +1,198 @@
+window.dataWilayah = window.dataWilayah || {
+      "kota-tangerang": {
+        slug: "kota-tangerang",
+        name: "Kota Tangerang",
+        type: "Kota",
+        kecamatan: {
+          "tangerang": {
+            name: "Tangerang",
+            deskripsi: "Pusat Kota Tangerang dan kawasan Pasar Lama dengan akses cepat Stasiun Tangerang.",
+            desa: ["Babakan", "Buaran Indah", "Cikokol", "Kelapa Indah", "Sukasari", "Sukarasa", "Sukaasih", "Tanah Tinggi"]
+          },
+          "karawaci": {
+            name: "Karawaci",
+            deskripsi: "Kawasan Lippo Village, kampus UPH, dan perumahan padat dengan layanan kurir jemput cepat.",
+            desa: ["Bojong Jaya", "Bugel", "Cimone", "Cimone Jaya", "Gerendeng", "Karawaci", "Karawaci Baru", "Koang Jaya", "Margasari", "Nambo Jaya", "Nusa Jaya", "Pabuaran", "Pabuaran Tumpeng", "Pasar Baru", "Sukajadi", "Sumur Pacing"]
+          },
+          "ciledug": {
+            name: "Ciledug",
+            deskripsi: "Kawasan hunian padat dan sentra niaga penghubung Tangerang-Jakarta Selatan.",
+            desa: ["Paninggilan", "Paninggilan Utara", "Parung Serab", "Sudimara Barat", "Sudimara Jaya", "Sudimara Selatan", "Sudimara Timur", "Tajur"]
+          },
+          "cipondoh": {
+            name: "Cipondoh",
+            deskripsi: "Kawasan sekitar Situ Cipondoh dan perumahan Banjar Wijaya, Poris Indah.",
+            desa: ["Cipondoh", "Cipondoh Indah", "Cipondoh Makmur", "Gondrong", "Kenanga", "Ketapang", "Petir", "Poris Plawad", "Poris Plawad Indah", "Poris Plawad Utara"]
+          },
+          "pinang": {
+            name: "Pinang",
+            deskripsi: "Kawasan berkembang pesat Alam Sutera dan Kunciran dengan akses tol langsung.",
+            desa: ["Cipete", "Kunciran", "Kunciran Indah", "Kunciran Jaya", "Nerogtog", "Pakojan", "Panunggangan", "Panunggangan Timur", "Panunggangan Utara", "Pinang", "Sudimara Pinang"]
+          },
+          "larangan": {
+            name: "Larangan",
+            deskripsi: "Perbatasan langsung dengan Puri Beta dan Jakarta Selatan dengan layanan kilat 6 jam.",
+            desa: ["Cipadu", "Cipadu Jaya", "Gaga", "Kreo", "Kreo Selatan", "Larangan Indah", "Larangan Selatan", "Larangan Utara"]
+          },
+          "karangtengah": {
+            name: "Karang Tengah",
+            deskripsi: "Area hunian modern sekitar Metro Permata dan Green Lake City.",
+            desa: ["Karang Mulya", "Karang Tengah", "Karang Timur", "Parung Jaya", "Pedurenan", "Pondok Bahar", "Pondok Pucung"]
+          },
+          "batuceper": {
+            name: "Batuceper",
+            deskripsi: "Kawasan industri dan pemukiman dekat stasiun transit Batuceper ke Bandara Soetta.",
+            desa: ["Batuceper", "Batujaya", "Batusari", "Kebon Besar", "Poris Gaga", "Poris Gaga Baru", "Poris Jaya"]
+          },
+          "benda": {
+            name: "Benda",
+            deskripsi: "Kawasan pintu gerbang Bandara Internasional Soekarno-Hatta dan perkantoran kargo.",
+            desa: ["Belendung", "Benda", "Jurumudi", "Jurumudi Baru", "Pajang"]
+          },
+          "cibodas": {
+            name: "Cibodas",
+            deskripsi: "Kawasan perumahan Taman Cibodas dan Perumnas dengan harga kiloan bersahabat.",
+            desa: ["Cibodas", "Cibodasari", "Cibodas Baru", "Jatiuwung", "Panbar", "Uwung Jaya"]
+          },
+          "jatiuwung": {
+            name: "Jatiuwung",
+            deskripsi: "Sentra kawasan industri barat Kota Tangerang dengan layanan 1 mesin 1 pelanggan.",
+            desa: ["Alam Jaya", "Gandasari", "Jatake", "Keroncong", "Manis Jaya", "Pasir Jaya"]
+          },
+          "periuk": {
+            name: "Periuk",
+            deskripsi: "Kawasan hunian Grand Duta, Villa Tangerang Regency, dan sekitarnya.",
+            desa: ["Gebang Raya", "Gembor", "Periuk", "Periuk Jaya", "Sangiang Jaya"]
+          },
+          "neglasari": {
+            name: "Neglasari",
+            deskripsi: "Kawasan dekat Bandara Soetta dengan armada kurir antar jemput gesit.",
+            desa: ["Karang Anyar", "Karangsari", "Kedaung Baru", "Kedaung Wetan", "Mekar Sari", "Neglasari", "Selapajang Jaya"]
+          }
+        }
+      },
+      "kota-tangerang-selatan": {
+        slug: "kota-tangerang-selatan",
+        name: "Kota Tangerang Selatan",
+        type: "Kota",
+        kecamatan: {
+          "serpong": {
+            name: "Serpong",
+            deskripsi: "Jantung kawasan mandiri BSD City dengan cluster hunian modern dan perkantoran.",
+            desa: ["Buaran", "Ciater", "Cilenggang", "Lengkong Gudang", "Lengkong Gudang Timur", "Lengkong Wetan", "Rawa Buntu", "Rawa Mekar Jaya", "Serpong"]
+          },
+          "serpong-utara": {
+            name: "Serpong Utara",
+            deskripsi: "Kawasan Alam Sutera, Graha Raya, dan Jelupang dengan layanan antar jemput express.",
+            desa: ["Jelupang", "Lengkong Karya", "Pakualam", "Pakulonan", "Paku Jaya", "Pondok Jagung", "Pondok Jagung Timur"]
+          },
+          "pondok-aren": {
+            name: "Pondok Aren",
+            deskripsi: "Kawasan Bintaro Jaya Sektor 1-9 dengan layanan cuci sepatu premium dan bedcover.",
+            desa: ["Jurang Mangu Barat", "Jurang Mangu Timur", "Pondok Kacang Barat", "Pondok Kacang Timur", "Perigi", "Perigi Baru", "Pondok Aren", "Pondok Betung", "Pondok Jaya", "Pondok Karya", "Pondok Pung"]
+          },
+          "ciputat": {
+            name: "Ciputat",
+            deskripsi: "Pusat aktivitas niaga dan kampus UIN Jakarta dengan tarif kiloan ramah mahasiswa.",
+            desa: ["Cipayung", "Ciputat", "Jombang", "Sawah Baru", "Sawah Lama", "Serua", "Serua Indah"]
+          },
+          "ciputat-timur": {
+            name: "Ciputat Timur",
+            deskripsi: "Kawasan hunian dekat Jakarta Selatan sekitar Cirendeu, Pisangan, dan Rempoa.",
+            desa: ["Cempaka Putih", "Cireundeu", "Pisangan", "Pondok Ranji", "Rempoa", "Rengas"]
+          },
+          "pamulang": {
+            name: "Pamulang",
+            deskripsi: "Kawasan pemukiman padat dan Universitas Pamulang (UNPAM) dengan sistem cuci higienis.",
+            desa: ["Bambu Apus", "Benda Baru", "Kedaung", "Pondok Benda", "Pamulang Barat", "Pamulang Timur", "Pondok Cabe Ilir", "Pondok Cabe Udik"]
+          },
+          "setu": {
+            name: "Setu",
+            deskripsi: "Pusat Pemerintahan Kota Tangsel dan kawasan Puspiptek dengan layanan jemput harian.",
+            desa: ["Babakan", "Bakti Jaya", "Kademangan", "Keranggan", "Muncul", "Setu"]
+          }
+        }
+      },
+      "kabupaten-tangerang": {
+        slug: "kabupaten-tangerang",
+        name: "Kabupaten Tangerang",
+        type: "Kabupaten",
+        kecamatan: {
+          "kelapa-dua": {
+            name: "Kelapa Dua",
+            deskripsi: "Kawasan elit Gading Serpong, Lippo Karawaci barat, dan Bojong Nangka.",
+            desa: ["Bencongan", "Bencongan Indah", "Bojong Nangka", "Curug Sangereng", "Kelapa Dua", "Pakulonan Barat"]
+          },
+          "curug": {
+            name: "Curug",
+            deskripsi: "Kawasan kampus STPI Curug dan perumahan berkembang di koridor barat.",
+            desa: ["Cukanggalih", "Curug Kulon", "Curug Wetan", "Kadu", "Kadu Jaya", "Sukabakti", "Binong"]
+          },
+          "pasar-kemis": {
+            name: "Pasar Kemis",
+            deskripsi: "Kawasan pemukiman terpadat sekitar Puri Jaya dan Kutabumi dengan harga terjangkau.",
+            desa: ["Gelam Jaya", "Kuta Baru", "Kutabumi", "Kuta Jaya", "Pangadegan", "Pasar Kemis", "Sindangsari", "Sukamantri"]
+          },
+          "cikupa": {
+            name: "Cikupa",
+            deskripsi: "Sentra industri dan kawasan mandiri CitraRaya Tangerang dengan layanan antar jemput.",
+            desa: ["Bitung Jaya", "Bojong", "Budi Mulya", "Cibadak", "Cikupa", "Dukuh", "Pasir Gadung", "Pasir Jaya", "Sukamulya", "Sukanagara", "Talaga", "Talagasari"]
+          },
+          "balaraja": {
+            name: "Balaraja",
+            deskripsi: "Pusat transit tol barat dan kawasan industri dengan standar cuci higienis 1 mesin 1 nota.",
+            desa: ["Balaraja", "Cangkudu", "Gembong", "Saga", "Sentul", "Sentul Jaya", "Sukamurni", "Talagasari", "Tobat"]
+          },
+          "cisauk": {
+            name: "Cisauk",
+            deskripsi: "Kawasan transit intermoda BSD dan stasiun Cisauk penghubung Tangerang-Bogor.",
+            desa: ["Cibogo", "Cisauk", "Dangdang", "Mekar Wangi", "Sampora", "Suradita"]
+          },
+          "pagedangan": {
+            name: "Pagedangan",
+            deskripsi: "Kawasan BSD City fase barat (ICE BSD, QBig, Vanya Park) dengan layanan express.",
+            desa: ["Cicalengka", "Cihuni", "Cijantra", "Jatake", "Kadu Sirung", "Karang Tengah", "Lengkong Kulon", "Medang", "Pagedangan", "Situgadung"]
+          },
+          "panongan": {
+            name: "Panongan",
+            deskripsi: "Kawasan hunian CitraRaya sektor selatan dan sekitarnya.",
+            desa: ["Ciakar", "Mekar Bakti", "Panongan", "Peusar", "Ranca Iyuh", "Ranca Kalapa", "Serdang Kulon"]
+          },
+          "legok": {
+            name: "Legok",
+            deskripsi: "Kawasan pemukiman strategis perbatasan Serpong-Parung Panjang.",
+            desa: ["Babat", "Bojongkamal", "Caringin", "Ciangir", "Cirarab", "Kamuning", "Legok", "Palasari", "Rancagong", "Serab"]
+          },
+          "tigaraksa": {
+            name: "Tigaraksa",
+            deskripsi: "Pusat Pemerintahan Kabupaten Tangerang dengan layanan prima cuci wangi bergaransi.",
+            desa: ["Bantar Panjang", "Cileles", "Cisereh", "Kadu Agung", "Margasari", "Matagara", "Pasir Bolang", "Pasir Nangka", "Pematang", "Petet", "Sodong", "Tegalsari", "Tigaraksa"]
+          },
+          "sepatan": {
+            name: "Sepatan",
+            deskripsi: "Kawasan pemukiman utara Tangerang dengan layanan antar jemput harian.",
+            desa: ["Karet", "Kayu Agung", "Kayu Bongkok", "Mekar Jaya", "Pisangan Jaya", "Sarakan", "Sepatan", "Kedaung Barat"]
+          },
+          "teluknaga": {
+            name: "Teluknaga",
+            deskripsi: "Kawasan pesisir utara dan PIK 2 dengan standar pengerjaan cepat dan rapi.",
+            desa: ["Babakan Asem", "Bojong Renged", "Kampung Besar", "Kampung Melayu Barat", "Kampung Melayu Timur", "Keboncau", "Lemo", "Muara", "Pangkalan", "Tanjung Burung", "Tanjung Pasir", "Teluknaga", "Tegal Angus"]
+          },
+          "kosambi": {
+            name: "Kosambi",
+            deskripsi: "Kawasan perbatasan Jakarta Barat dan pengembangan PIK 2 dengan kurir jemput gesit.",
+            desa: ["Belimbing", "Cengklong", "Dadap", "Jati Mulya", "Kosambi Barat", "Kosambi Timur", "Rawa Burung", "Rawa Rengas", "Salembaran Jati", "Salembaran Jaya"]
+          },
+          "rajeg": {
+            name: "Rajeg",
+            deskripsi: "Kawasan hunian perumahan baru berkembang pesat dengan paket kiloan hemat.",
+            desa: ["Daon", "Jambu Karya", "Lembangsari", "Mekarsari", "Pangarengan", "Rajeg", "Rajeg Mulya", "Ranca Bango", "Sukatani", "Tanjakan", "Tanjakan Mekar"]
+          },
+          "sindang-jaya": {
+            name: "Sindang Jaya",
+            deskripsi: "Kawasan kota mandiri Suvarna Sutera dengan layanan laundry modern berkelas.",
+            desa: ["Badak Anom", "Sindang Asih", "Sindang Jaya", "Sindang Panon", "Sindang Sono", "Sukaharja", "Wanakerta"]
+          }
+        }
+      }
+    };
