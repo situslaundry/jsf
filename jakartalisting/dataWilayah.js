359f86697dc2e0e1bf10f5596b60d6c1a9dc0269
situslaundry <situslaundry@gmail.com>
2026-08-18 20:31:34 +0700

    Create dataWilayah.js

---
+++ jakartalisting/dataWilayah.js
@@ -0,0 +1,79 @@
+window.dataWilayah = window.dataWilayah || {
+      "jakarta-selatan": {
+        slug: "jakarta-selatan",
+        name: "Jakarta Selatan",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "tebet": { name: "Tebet", deskripsi: "Kawasan hunian strategis, kuliner, dan sentra stasiun transit.", desa: ["Bukit Duri", "Kebon Baru", "Manggarai", "Manggarai Selatan", "Menteng Dalam", "Tebet Barat", "Tebet Timur"] },
+          "setiabudi": { name: "Setiabudi", deskripsi: "Pusat bisnis segitiga emas Kuningan, Sudirman, dan apartemen modern.", desa: ["Guntur", "Karet", "Karet Kuningan", "Karet Semanggi", "Kuningan Timur", "Menteng Atas", "Pasar Manggis", "Setiabudi"] },
+          "mampang-prapatan": { name: "Mampang Prapatan", deskripsi: "Kawasan perkantoran koridor Mampang, Tendean, dan Kemang utara.", desa: ["Bangka", "Kuningan Barat", "Mampang Prapatan", "Pela Mampang", "Tegal Parang"] },
+          "kebayoran-baru": { name: "Kebayoran Baru", deskripsi: "Pusat gaya hidup Senopati, SCBD, Blok M, Gandaria, dan Dharmawangsa.", desa: ["Cipete Utara", "Gandaria Utara", "Gunung", "Kramat Pela", "Melawai", "Petogogan", "Pulo", "Rawa Barat", "Selong", "Senayan"] },
+          "kebayoran-lama": { name: "Kebayoran Lama", deskripsi: "Kawasan hunian Pondok Indah, Gandaria City, dan Permata Hijau.", desa: ["Cipulir", "Grogol Selatan", "Grogol Utara", "Kebayoran Lama Selatan", "Kebayoran Lama Utara", "Pondok Pinang"] },
+          "cilandak": { name: "Cilandak", deskripsi: "Kawasan koridor TB Simatupang, Cilandak Town Square, dan Fatmawati.", desa: ["Cilandak Barat", "Cipete Selatan", "Gandaria Selatan", "Lebak Bulus", "Pondok Labu"] },
+          "pasar-minggu": { name: "Pasar Minggu", deskripsi: "Kawasan pemukiman Pejaten, Ragunan, dan perkantoran Simatupang.", desa: ["Cilandak Timur", "Jati Padang", "Kebagusan", "Pasar Minggu", "Pejaten Barat", "Pejaten Timur", "Ragunan"] },
+          "pancoran": { name: "Pancoran", deskripsi: "Kawasan transit Kalibata, Pengadegan, dan apartemen padat.", desa: ["Cikoko", "Duren Tiga", "Kalibata", "Pancoran", "Pengadegan", "Rawajati"] },
+          "jagakarsa": { name: "Jagakarsa", deskripsi: "Kawasan hunian asri Lenteng Agung, Ciganjur, dan sekitar kampus UI/ISTN.", desa: ["Ciganjur", "Cipedak", "Jagakarsa", "Lenteng Agung", "Srengseng Sawah", "Tanjung Barat"] },
+          "pesanggrahan": { name: "Pesanggrahan", deskripsi: "Kawasan Bintaro sektor utara, Petukangan, dan Ulujami.", desa: ["Bintaro", "Pesanggrahan", "Petukangan Selatan", "Petukangan Utara", "Ulujami"] }
+        }
+      },
+      "jakarta-barat": {
+        slug: "jakarta-barat",
+        name: "Jakarta Barat",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "kebon-jeruk": { name: "Kebon Jeruk", deskripsi: "Kawasan hunian Puri, Kedoya, dan kantor stasiun televisi nasional.", desa: ["Duri Kepa", "Kebon Jeruk", "Kedoya Selatan", "Kedoya Utara", "Kelapa Dua", "Sukabumi Selatan", "Sukabumi Utara"] },
+          "grogol-petamburan": { name: "Grogol Petamburan", deskripsi: "Kawasan kampus Trisakti, Untar, Mall Central Park, dan Ciputra Mall.", desa: ["Grogol", "Jelambar", "Jelambar Baru", "Tanjung Duren Selatan", "Tanjung Duren Utara", "Tomang", "Wijaya Kusuma"] },
+          "kembangan": { name: "Kembangan", deskripsi: "Pusat Pemerintahan Jakarta Barat, Puri Indah, dan akses tol JORR.", desa: ["Joglo", "Kembangan Selatan", "Kembangan Utara", "Meruya Selatan", "Meruya Utara", "Srengseng"] },
+          "palmerah": { name: "Palmerah", deskripsi: "Kawasan dekat stasiun Palmerah, Slipi, dan kampus Binus Kemanggisan.", desa: ["Jatipulo", "Kemanggisan", "Kota Bambu Selatan", "Kota Bambu Utara", "Palmerah", "Slipi"] },
+          "cengkareng": { name: "Cengkareng", deskripsi: "Kawasan perumahan Taman Palem, Duri Kosambi, dan Rawa Buaya.", desa: ["Cengkareng Barat", "Cengkareng Timur", "Duri Kosambi", "Kapuk", "Kedaung Kali Angke", "Rawa Buaya"] },
+          "kalideres": { name: "Kalideres", deskripsi: "Kawasan hunian Citra Garden, Daan Mogot, dan Terminal Kalideres.", desa: ["Kalideres", "Kamal", "Pegadungan", "Semanan", "Tegal Alur"] },
+          "tamansari": { name: "Tamansari", deskripsi: "Kawasan Kota Tua, Glodok, dan sentra perdagangan Mangga Besar.", desa: ["Glodok", "Keagungan", "Krukut", "Mangga Besar", "Maphar", "Pinangsia", "Taman Sari", "Tangki"] },
+          "tambora": { name: "Tambora", deskripsi: "Kawasan niaga Pasar Pagi, Jembatan Lima, dan Angke.", desa: ["Angke", "Duri Selatan", "Duri Utara", "Jembatan Besi", "Jembatan Lima", "Kali Anyar", "Krendang", "Pekojan", "Roa Malaka", "Tambora", "Tanah Sereal"] }
+        }
+      },
+      "jakarta-pusat": {
+        slug: "jakarta-pusat",
+        name: "Jakarta Pusat",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "menteng": { name: "Menteng", deskripsi: "Kawasan hunian eksklusif diplomatik, Cikini, dan Jalan Thamrin.", desa: ["Cikini", "Gondangdia", "Kebon Sirih", "Menteng", "Pegangsaan"] },
+          "tanah-abang": { name: "Tanah Abang", deskripsi: "Sentra tekstil terbesar, Bundaran HI, Bendungan Hilir, dan Petamburan.", desa: ["Bendungan Hilir", "Gelora", "Kampung Bali", "Karet Tengsin", "Kebon Kacang", "Kebon Melati", "Petamburan"] },
+          "gambir": { name: "Gambir", deskripsi: "Kawasan Monas, Istana Negara, Balai Kota, dan Stasiun Gambir.", desa: ["Cideng", "Duri Pulo", "Gambir", "Kebon Kelapa", "Petojo Selatan", "Petojo Utara"] },
+          "kemayoran": { name: "Kemayoran", deskripsi: "Kawasan JIExpo Kemayoran, apartemen Menara Jakarta, dan perumahan.", desa: ["Cempaka Baru", "Gunung Sahari Selatan", "Harapan Mulya", "Kebon Kosong", "Kemayoran", "Serdang", "Sumur Batu", "Utan Panjang"] },
+          "cempaka-putih": { name: "Cempaka Putih", deskripsi: "Kawasan hunian tenang Cempaka Putih Timur dan Barat dekat Rawasari.", desa: ["Cempaka Putih Barat", "Cempaka Putih Timur", "Rawasari"] },
+          "senen": { name: "Senen", deskripsi: "Pusat transit Stasiun Pasar Senen, Kwitang, dan RSPAD Gatot Soebroto.", desa: ["Bungur", "Kenari", "Kramat", "Kwitang", "Paseban", "Senen"] },
+          "sawah-besar": { name: "Sawah Besar", deskripsi: "Kawasan Pasar Baru, Mangga Dua Selatan, dan Masjid Istiqlal.", desa: ["Gunung Sahari Utara", "Karang Anyar", "Kartini", "Mangga Dua Selatan", "Pasar Baru"] },
+          "johor-baru": { name: "Johar Baru", deskripsi: "Kawasan pemukiman padat di koridor timur Jakarta Pusat.", desa: ["Galur", "Johar Baru", "Kampung Rawa", "Tanah Tinggi"] }
+        }
+      },
+      "jakarta-timur": {
+        slug: "jakarta-timur",
+        name: "Jakarta Timur",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "matraman": { name: "Matraman", deskripsi: "Pintu masuk Jakarta Timur sekitar Pramuka dan Utan Kayu.", desa: ["Kayu Manis", "Kebon Manggis", "Pal Meriam", "Pisangan Baru", "Utan Kayu Selatan", "Utan Kayu Utara"] },
+          "pulo-gadung": { name: "Pulo Gadung", deskripsi: "Kawasan Rawamangun, Velodrome, Pacuan Kuda, dan Kawasan Industri JIEP.", desa: ["Cipinang", "Jati", "Jatinegara Kaum", "Kayu Putih", "Pisangan Timur", "Pulo Gadung", "Rawamangun"] },
+          "jatinegara": { name: "Jatinegara", deskripsi: "Pusat transit Stasiun Jatinegara, Kampung Melayu, dan Otista.", desa: ["Bali Mester", "Bidara Cina", "Cipinang Besar Selatan", "Cipinang Besar Utara", "Cipinang Cempedak", "Cipinang Muara", "Kampung Melayu", "Rawa Bunga"] },
+          "duren-sawit": { name: "Duren Sawit", deskripsi: "Kawasan pemukiman mapan Buaran, Klender, dan Pondok Kelapa.", desa: ["Duren Sawit", "Klender", "Malaka Jaya", "Malaka Sari", "Pondok Bambu", "Pondok Kelapa", "Pondok Kopi"] },
+          "kramat-jati": { name: "Kramat Jati", deskripsi: "Sentra Pasar Induk Kramat Jati, Cililitan, dan Cawang transit.", desa: ["Batu Ampar", "Balekambang", "Cawang", "Cililitan", "Dukuh", "Kramat Jati", "Tengah"] },
+          "makasar": { name: "Makasar", deskripsi: "Kawasan Bandara Halim Perdanakusuma, Pinang Ranti, dan TMII utara.", desa: ["Cipinang Melayu", "Halim Perdana Kusumah", "Kebon Pala", "Makasar", "Pinang Ranti"] },
+          "pasar-rebo": { name: "Pasar Rebo", deskripsi: "Kawasan hunian Kalisari, Cijantung, dan industri Jl. Raya Bogor.", desa: ["Baru", "Cijantung", "Gedong", "Kalisari", "Pekayon"] },
+          "ciracas": { name: "Ciracas", deskripsi: "Kawasan LRT Cibubur, Kelapa Dua Wetan, dan Kampung Rambutan.", desa: ["Cibubur", "Ciracas", "Kelapa Dua Wetan", "Rambutan", "Susukan"] },
+          "cipayung": { name: "Cipayung", deskripsi: "Kawasan hijau TMII, Bambu Apus, Lubang Buaya, dan Cilangkap.", desa: ["Bambu Apus", "Ceger", "Cilangkap", "Cipayung", "Lubang Buaya", "Munjul", "Pondok Ranggon", "Setu"] },
+          "cakung": { name: "Cakung", deskripsi: "Pusat Pemerintahan Jakarta Timur, Jakarta Garden City, dan Penggilingan.", desa: ["Cakung Barat", "Cakung Timur", "Jatinegara", "Penggilingan", "Pulo Gebang", "Rawa Terate", "Ujung Menteng"] }
+        }
+      },
+      "jakarta-utara": {
+        slug: "jakarta-utara",
+        name: "Jakarta Utara",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "kelapa-gading": { name: "Kelapa Gading", deskripsi: "Kawasan elit perumahan, Mall Kelapa Gading, dan sentra kuliner.", desa: ["Kelapa Gading Barat", "Kelapa Gading Timur", "Pegangsaan Dua"] },
+          "tanjung-priok": { name: "Tanjung Priok", deskripsi: "Pusat pelabuhan utama Tanjung Priok, Sunter Agung, dan JIS.", desa: ["Kebon Bawang", "Papanggo", "Sungai Bambu", "Sunter Agung", "Sunter Jaya", "Tanjung Priok", "Warakas"] },
+          "pademangan": { name: "Pademangan", deskripsi: "Kawasan wisata Ancol Taman Impian, Mangga Dua, dan Pademangan.", desa: ["Ancol", "Pademangan Barat", "Pademangan Timur"] },
+          "penjaringan": { name: "Penjaringan", deskripsi: "Kawasan Pantai Indah Kapuk (PIK), Pluit, Muara Karang, dan Kapuk Muara.", desa: ["Kamal Muara", "Kapuk Muara", "Pejagalan", "Penjaringan", "Pluit"] },
+          "koja": { name: "Koja", deskripsi: "Kawasan sentra Jakarta Islamic Centre, Lagoa, dan Rawa Badak.", desa: ["Koja", "Lagoa", "Rawa Badak Selatan", "Rawa Badak Utara", "Tugu Selatan", "Tugu Utara"] },
+          "cilincing": { name: "Cilincing", deskripsi: "Kawasan industri Marunda, Rorotan, dan pesisir timur Jakarta Utara.", desa: ["Cilincing", "Kalibaru", "Marunda", "Rorotan", "Semper Barat", "Semper Timur", "Sukapura"] }
+        }
+      }
+    };
