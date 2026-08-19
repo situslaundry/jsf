0b9f44eb6ba5c375400716a2da6dc457e5509573
situslaundry <situslaundry@gmail.com>
2026-08-19 13:55:43 +0700

    Create config.js

---
+++ bandarlaundrylisting/config.js
@@ -0,0 +1,20 @@
+ window.SITE_CONFIG = window.SITE_CONFIG || {
+      defaultPhone: "085773009666",
+      phoneRouting: {
+        "jakarta": "085771419529",
+        "jakarta-selatan": "085771419529",
+        "jakarta-barat": "085771419529",
+        "jakarta-pusat": "085771419529",
+        "jakarta-timur": "085771419529",
+        "jakarta-utara": "085771419529",
+        "kabupaten-banyumas": "085773009666",
+        "kabupaten-bogor": "085773009666",
+        "kota-bogor": "085773009666",
+        "kota-depok": "085773009666",
+        "kota-tangerang": "085773009666",
+        "kota-tangerang-selatan": "085773009666",
+        "kabupaten-tangerang": "085773009666",
+        "kota-bekasi": "085773009666",
+        "kabupaten-bekasi": "085773009666"
+      }
+    };
