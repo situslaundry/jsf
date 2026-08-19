acde65417c284081f8b03e34ab3818a742919536
situslaundry <situslaundry@gmail.com>
2026-08-19 13:57:16 +0700

    Create dataWilayah.js

---
+++ bandarlaundrylisting/dataWilayah.js
@@ -0,0 +1,165 @@
+window.dataWilayah = window.dataWilayah || {
+      // 1. KABUPATEN BANYUMAS (27 KECAMATAN LENGKAP)
+      "kabupaten-banyumas": {
+        slug: "kabupaten-banyumas",
+        name: "Kabupaten Banyumas",
+        type: "Kabupaten",
+        kecamatan: {
+          "sokaraja": { name: "Sokaraja", deskripsi: "Pusat kuliner getuk goreng dan kawasan hunian strategis Banyumas.", desa: ["Banjaranyar", "Banjarsari Kidul", "Jompo Kulon", "Kalikidang", "Karangduren", "Karangkedawung", "Karangnanas", "Karangrau", "Kedondong", "Klahang", "Lemberang", "Pamijen", "Sokaraja Kidul", "Sokaraja Kulon", "Sokaraja Lor", "Sokaraja Tengah", "Sokaraja Wetan", "Wiradadi"] },
+          "purwokerto-timur": { name: "Purwokerto Timur", deskripsi: "Pusat perkantoran, perhotelan, dan pertokoan Purwokerto.", desa: ["Arcawinangun", "Kranji", "Mersi", "Purwokerto Lor", "Purwokerto Wetan", "Sokanegara"] },
+          "purwokerto-barat": { name: "Purwokerto Barat", deskripsi: "Kawasan Stasiun Besar Purwokerto dan sentra hunian.", desa: ["Bantarsoka", "Karanglewas Lor", "Kedungwringin", "Kober", "Pasir Kidul", "Pasirmuncang", "Rejasari"] },
+          "purwokerto-selatan": { name: "Purwokerto Selatan", deskripsi: "Kawasan sekitar Terminal Bulupitu dan sentra niaga.", desa: ["Berkoh", "Karangklesem", "Karangpucung", "Purwokerto Kidul", "Purwokerto Kulon", "Tanjung", "Teluk"] },
+          "purwokerto-utara": { name: "Purwokerto Utara", deskripsi: "Kawasan pendidikan dan kampus UNSOED.", desa: ["Bancarkembar", "Bobosan", "Grendeng", "Karangwangkal", "Pabuaran", "Purwanegara", "Sumampir"] },
+          "ajibarang": { name: "Ajibarang", deskripsi: "Jalur persimpangan utama barat Banyumas.", desa: ["Ajibarang Kulon", "Ajibarang Wetan", "Ciberung", "Darmakradenan", "Jingkang", "Kalibenda", "Karangkemiri", "Kracak", "Lesmana", "Pancasan", "Pancurendang", "Pandansari", "Sawangan", "Tipar Kidul", "Windujaya"] },
+          "baturraden": { name: "Baturraden", deskripsi: "Kawasan wisata lereng Gunung Slamet, hotel, dan villa.", desa: ["Karangmangu", "Karangsalam Lor", "Karangtengah", "Kebumen", "Kemutug Kidul", "Kemutug Lor", "Ketenger", "Kutasari", "Pandak", "Pamijen", "Rempoah", "Purwosari"] },
+          "banyumas": { name: "Banyumas", deskripsi: "Kota lama bersejarah dan pusat kebudayaan Banyumas.", desa: ["Binangun", "Danaraja", "Dawuhan", "Gedunguter", "Kalisube", "Karangrau", "Kedunggede", "Kedunguter", "Kejawar", "Papringan", "Pekunden", "Sudagaran"] },
+          "cilongok": { name: "Cilongok", deskripsi: "Kecamatan terluas dan sentra industri gula kelapa.", desa: ["Batuanten", "Cikidang", "Cilongok", "Cipete", "Gunungsari", "Jatisaba", "Kalisari", "Karanglo", "Karangtengah", "Kasegeran", "Langkiteng", "Pancasandana", "Panembangan", "Pejogol", "Pernasidi", "Rancamaya", "Sambirata", "Sudimara"] },
+          "gumelar": { name: "Gumelar", deskripsi: "Wilayah perbukitan barat laut Banyumas.", desa: ["Cihonje", "Cilangkap", "Gandoang", "Gumelar", "Karangkemojing", "Kedungurang", "Samudra", "Samudra Kulon", "Tlaga"] },
+          "kalibagor": { name: "Kalibagor", deskripsi: "Area pertanian dan pemukiman timur Banyumas.", desa: ["Kalibagor", "Kaliori", "Kalisogra Wetan", "Karangdadap", "Pajerukan", "Petir", "Srowot", "Suro", "Wlahar Wetan"] },
+          "karanglewas": { name: "Karanglewas", deskripsi: "Kecamatan penyangga barat Purwokerto.", desa: ["Babakan", "Jipang", "Karanggude Kulon", "Karangkemiri", "Karanglewas Kidul", "Kediri", "Pangebatan", "Pasir Kulon", "Pasir Lor", "Pasir Wetan", "Singasari", "Sunyalangu", "Tamansari"] },
+          "kebasen": { name: "Kebasen", deskripsi: "Kawasan tepi Sungai Serayu dengan jalur strategis.", desa: ["Bangsa", "Gambarsari", "Kalisalak", "Kaliwedi", "Kebasen", "Mandirancan", "Randegan", "Sawangan", "Tumiyang"] },
+          "kedungbanteng": { name: "Kedungbanteng", deskripsi: "Area sejuk lereng selatan Gunung Slamet.", desa: ["Beji", "Dawuhan Kulon", "Dawuhan Wetan", "Kalikesur", "Kalisalak", "Karangnangka", "Karangsalam Kidul", "Kebocoran", "Kedungbanteng", "Keniten", "Kutaliman", "Melung", "Windujaya", "Baseh"] },
+          "kembaran": { name: "Kembaran", deskripsi: "Dekat kampus UMP dan kawasan pemukiman padat.", desa: ["Bantarwuni", "Bojongsari", "Dukuhwaluh", "Karangsari", "Karangsoka", "Karangtengah", "Kembaran", "Kramat", "Ledug", "Linggasari", "Pliken", "Purbadana", "Purwodadi", "Sambeng Kulon", "Sambeng Wetan", "Tambaksari Kidul"] },
+          "kemranjen": { name: "Kemranjen", deskripsi: "Sentra durian bhineka bawor Banyumas.", desa: ["Alasmalang", "Bagasari", "Giri Kencana", "Karanggintung", "Karangjati", "Karangsalam", "Kebarongan", "Kecila", "Kedungpring", "Nusamangir", "Pageralang", "Petarangan", "Sibalung", "Sibrama", "Sirau"] },
+          "jatilawang": { name: "Jatilawang", deskripsi: "Sentra perdagangan barat daya Banyumas.", desa: ["Bantar", "Gentawangi", "Gunung Wetan", "Karanganyar", "Karanglewas", "Kedungwringin", "Margasana", "Pekuncen", "Rawalo", "Tinggarjaya", "Tunjung"] },
+          "lumbir": { name: "Lumbir", deskripsi: "Kecamatan perbatasan Banyumas-Cilacap/Jabar.", desa: ["Besuki", "Canduk", "Cidora", "Cingebul", "Cirahab", "Dermaji", "Karanggayam", "Kedunggede", "Lumbir", "Parungkamal"] },
+          "patikraja": { name: "Patikraja", deskripsi: "Akses utama selatan Kota Purwokerto.", desa: ["Kedungrandu", "Kedungwringin", "Kedungwuluh Kidul", "Kedungwuluh Lor", "Notog", "Patikraja", "Pegangsalan", "Sawangan Wetan", "Sidabowa", "Sokawera", "Wlahar Kulon"] },
+          "pekuncen": { name: "Pekuncen", deskripsi: "Jalur lintas utara Banyumas penghubung Brebes/Tegal.", desa: ["Banjaranyar", "Candinegara", "Cibangkong", "Cikawung", "Cikembulan", "Glempang", "Karangkemiri", "Karangklesem", "Krajan", "Kranggan", "Pasiraman Kidul", "Pasiraman Lor", "Pekuncen", "Petahunan", "Semangkung", "Tumiyang"] },
+          "purwojati": { name: "Purwojati", deskripsi: "Kawasan perbukitan asri Banyumas barat.", desa: ["Galisalak", "Kaliiteng", "Kaliwangi", "Karangmangu", "Karangtalun Kidul", "Karangtalun Lor", "Klapagading", "Klapagading Kulon", "Purwojati"] },
+          "rawalo": { name: "Rawalo", deskripsi: "Persimpangan jalur utama selatan Banyumas.", desa: ["Banjarparakan", "Losari", "Menganti", "Pesawahan", "Rawalo", "Sanggreman", "Sidamulih", "Sukoharjo", "Tambaknegara"] },
+          "somagede": { name: "Somagede", deskripsi: "Wilayah timur Banyumas dengan sentra pertanian.", desa: ["Kanding", "Kemawi", "Klinting", "Piasa Kulon", "Plana", "Sokawera", "Somagede", "Tanggeran"] },
+          "sumbang": { name: "Sumbang", deskripsi: "Kawasan hunian dan instansi timur Purwokerto.", desa: ["Banjarsari Kulon", "Banjarsari Wetan", "Ciberem", "Datar", "Gandatapa", "Karangcegak", "Karanggintung", "Karangturi", "Kawungcarang", "Kebanggan", "Kedungmalang", "Kotayasa", "Limbangan", "Sikapat", "Silado", "Sumbang", "Susukan", "Tambaksogra"] },
+          "sumpiuh": { name: "Sumpiuh", deskripsi: "Kota transit penting di tenggara Banyumas.", desa: ["Bogangin", "Karanggedang", "Kemiri", "Ketinggem", "Kradenan", "Kuntili", "Lebeng", "Nusadadi", "Pandak", "Selandaka", "Sumpiuh"] },
+          "tambak": { name: "Tambak", deskripsi: "Ujung timur Banyumas sentra sate bebek.", desa: ["Buniayu", "Gebangsari", "Gumelar Kidul", "Gumelar Lor", "Kamulyan", "Karangpetir", "Karangpucung", "Plangkapan", "Prembun", "Purwodadi", "Watuagung"] },
+          "wangon": { name: "Wangon", deskripsi: "Pusat aktivitas ekonomi barat daya Banyumas.", desa: ["Banteran", "Cikakak", "Jambu", "Jurangbahas", "Klapagading", "Klapagading Kulon", "Pangadegan", "Randegan", "Rawaheng", "Wangon", "Windunegara", "Wlahar"] }
+        }
+      },
+
+      // 2. JAKARTA
+      "jakarta-selatan": {
+        slug: "jakarta-selatan",
+        name: "Jakarta Selatan",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "tebet": { name: "Tebet", deskripsi: "Kawasan hunian strategis dan transit.", desa: ["Bukit Duri", "Kebon Baru", "Manggarai", "Menteng Dalam", "Tebet Barat", "Tebet Timur"] },
+          "setiabudi": { name: "Setiabudi", deskripsi: "Pusat bisnis Kuningan dan Sudirman.", desa: ["Karet Kuningan", "Kuningan Timur", "Menteng Atas", "Setiabudi"] },
+          "kebayoran-baru": { name: "Kebayoran Baru", deskripsi: "Pusat gaya hidup Senopati dan Blok M.", desa: ["Cipete Utara", "Gandaria Utara", "Melawai", "Petogogan", "Senayan"] }
+        }
+      },
+      "jakarta-barat": {
+        slug: "jakarta-barat",
+        name: "Jakarta Barat",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "kebon-jeruk": { name: "Kebon Jeruk", deskripsi: "Kawasan Puri dan Kedoya.", desa: ["Duri Kepa", "Kebon Jeruk", "Kedoya Selatan", "Kedoya Utara"] },
+          "grogol-petamburan": { name: "Grogol Petamburan", deskripsi: "Kawasan kampus Trisakti dan Mall Central Park.", desa: ["Grogol", "Jelambar", "Tanjung Duren Selatan", "Tomang"] }
+        }
+      },
+      "jakarta-pusat": {
+        slug: "jakarta-pusat",
+        name: "Jakarta Pusat",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "menteng": { name: "Menteng", deskripsi: "Kawasan eksklusif diplomatik dan Thamrin.", desa: ["Cikini", "Gondangdia", "Kebon Sirih", "Menteng"] },
+          "tanah-abang": { name: "Tanah Abang", deskripsi: "Sentra tekstil dan Bundaran HI.", desa: ["Bendungan Hilir", "Gelora", "Kampung Bali", "Petamburan"] }
+        }
+      },
+      "jakarta-timur": {
+        slug: "jakarta-timur",
+        name: "Jakarta Timur",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "pulo-gadung": { name: "Pulo Gadung", deskripsi: "Kawasan Rawamangun dan Velodrome.", desa: ["Cipinang", "Jati", "Kayu Putih", "Rawamangun"] },
+          "duren-sawit": { name: "Duren Sawit", deskripsi: "Kawasan pemukiman Buaran dan Klender.", desa: ["Duren Sawit", "Klender", "Pondok Bambu", "Pondok Kelapa"] }
+        }
+      },
+      "jakarta-utara": {
+        slug: "jakarta-utara",
+        name: "Jakarta Utara",
+        type: "Kota Administrasi",
+        kecamatan: {
+          "kelapa-gading": { name: "Kelapa Gading", deskripsi: "Kawasan elit perumahan dan mall.", desa: ["Kelapa Gading Barat", "Kelapa Gading Timur", "Pegangsaan Dua"] },
+          "penjaringan": { name: "Penjaringan", deskripsi: "Kawasan PIK dan Pluit.", desa: ["Kamal Muara", "Kapuk Muara", "Pejagalan", "Pluit"] }
+        }
+      },
+
+      // 3. BOGOR
+      "kabupaten-bogor": {
+        slug: "kabupaten-bogor",
+        name: "Kabupaten Bogor",
+        type: "Kabupaten",
+        kecamatan: {
+          "cileungsi": { name: "Cileungsi", deskripsi: "Kawasan hunian dan industri timur Bogor.", desa: ["Pasirangin", "Cileungsi Kidul", "Limusnunggal", "Cipenjo", "Mekarsari", "Gandoang"] },
+          "cibinong": { name: "Cibinong", deskripsi: "Pusat pemerintahan Kabupaten Bogor.", desa: ["Cirimekar", "Ciriung", "Harapan Jaya", "Pakansari", "Sukahati"] }
+        }
+      },
+      "kota-bogor": {
+        slug: "kota-bogor",
+        name: "Kota Bogor",
+        type: "Kota",
+        kecamatan: {
+          "bogor-tengah": { name: "Bogor Tengah", deskripsi: "Pusat Kota Bogor di sekeliling Kebun Raya.", desa: ["Babakan", "Cibogor", "Pabaton", "Paledang", "Sempur"] },
+          "bogor-barat": { name: "Bogor Barat", deskripsi: "Kawasan niaga dan pemukiman Yasmin.", desa: ["Bubulak", "Cilendek Barat", "Menteng", "Semplak"] }
+        }
+      },
+
+      // 4. DEPOK
+      "kota-depok": {
+        slug: "kota-depok",
+        name: "Kota Depok",
+        type: "Kota",
+        kecamatan: {
+          "beji": { name: "Beji", deskripsi: "Kawasan kampus UI dan Margonda Raya.", desa: ["Beji", "Kemiri Muka", "Kukusan", "Pondok Cina", "Tanah Baru"] },
+          "pancoran-mas": { name: "Pancoran Mas", deskripsi: "Pusat kota Depok Lama dan stasiun.", desa: ["Depok", "Depok Jaya", "Mampang", "Pancoran Mas"] }
+        }
+      },
+
+      // 5. TANGERANG
+      "kota-tangerang": {
+        slug: "kota-tangerang",
+        name: "Kota Tangerang",
+        type: "Kota",
+        kecamatan: {
+          "tangerang": { name: "Tangerang", deskripsi: "Pusat kota Tangerang dan Pasar Lama.", desa: ["Babakan", "Buaran Indah", "Cikokol", "Sukasari"] },
+          "karawaci": { name: "Karawaci", deskripsi: "Kawasan Lippo Village dan kampus UPH.", desa: ["Bojong Jaya", "Cimone", "Karawaci", "Sukajadi"] }
+        }
+      },
+      "kota-tangerang-selatan": {
+        slug: "kota-tangerang-selatan",
+        name: "Kota Tangerang Selatan",
+        type: "Kota",
+        kecamatan: {
+          "serpong": { name: "Serpong", deskripsi: "Kawasan mandiri BSD City.", desa: ["Ciater", "Cilenggang", "Lengkong Gudang", "Rawa Buntu", "Serpong"] },
+          "pondok-aren": { name: "Pondok Aren", deskripsi: "Kawasan Bintaro Jaya Sektor 1-9.", desa: ["Jurang Mangu Barat", "Pondok Betung", "Pondok Jaya", "Pondok Ranji"] }
+        }
+      },
+      "kabupaten-tangerang": {
+        slug: "kabupaten-tangerang",
+        name: "Kabupaten Tangerang",
+        type: "Kabupaten",
+        kecamatan: {
+          "kelapa-dua": { name: "Kelapa Dua", deskripsi: "Kawasan Gading Serpong.", desa: ["Bencongan", "Bojong Nangka", "Curug Sangereng", "Kelapa Dua"] },
+          "cikupa": { name: "Cikupa", deskripsi: "Kawasan industri dan CitraRaya.", desa: ["Bitung Jaya", "Bojong", "Cikupa", "Talaga"] }
+        }
+      },
+
+      // 6. BEKASI
+      "kota-bekasi": {
+        slug: "kota-bekasi",
+        name: "Kota Bekasi",
+        type: "Kota",
+        kecamatan: {
+          "bekasi-barat": { name: "Bekasi Barat", deskripsi: "Kawasan niaga dan transit Stasiun Kranji.", desa: ["Bintara", "Bintara Jaya", "Jaka Sampurna", "Kranji"] },
+          "bekasi-selatan": { name: "Bekasi Selatan", deskripsi: "Pusat bisnis Grand Metropolitan dan Galaxy.", desa: ["Jaka Mulya", "Kayuringin Jaya", "Pekayon Jaya"] }
+        }
+      },
+      "kabupaten-bekasi": {
+        slug: "kabupaten-bekasi",
+        name: "Kabupaten Bekasi",
+        type: "Kabupaten",
+        kecamatan: {
+          "tambun-selatan": { name: "Tambun Selatan", deskripsi: "Kawasan pemukiman dekat Grand Wisata.", desa: ["Jatimulya", "Lambangjaya", "Lambangsari", "Tambun"] },
+          "cikarang-selatan": { name: "Cikarang Selatan", deskripsi: "Kawasan Lippo Cikarang dan EJIP.", desa: ["Ciantra", "Cibatu", "Pasirsari", "Sukadami"] }
+        }
+      }
+    };
